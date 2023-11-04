10ac29cad429eaa230aff7e398877fbc1f544d6b
ALucasE <alucase@gmail.com>
2023-11-04 19:27:45 -0300

    feat: se agrega inicio de sesion
    Queda funcionando el inicio de sesión y la autenticación junto con el uso de los roles

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 import { validarUsuario, erroresValidacion } from "../middleware/user.validations.js";
 const authRouters = Router();
 
 authRouters.post("/signup", validarUsuario, erroresValidacion, signup);
+authRouters.post("/signin", signin);
 
 export default authRouters;
--- src/routers/product.routes.js
+++ src/routers/product.routes.js
@@ -2,9 +2,10 @@
 import { Router } from "express";
 const productRoutes = Router();
 import { getAllPorducts, getPorductsById, ugradePorduct, deleteProduct, createPorduct } from "../controllers/product.controller.js";
+import { userAuth, isAdmin } from "../middleware/user.auth.jwt.js";
 
 /*- - - - - - - - /products - - - - - - - -*/
-productRoutes.get("/", getAllPorducts);
+productRoutes.get("/", userAuth, isAdmin, getAllPorducts);
 productRoutes.get("/:productId", getPorductsById);
 productRoutes.post("/", createPorduct);
 productRoutes.put("/:productId", ugradePorduct);
